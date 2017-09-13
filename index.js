ac9c9d321d8b8bfff8407a1509cc5d73fc4136ce
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-09-13 16:46:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = new Object();
 
 function updateObjectWithKeyAndValue(object, key, value){
-  var newRecipes = Object.assign({},object,{key:value});
+  var newRecipes = Object.assign({},object,{[key]:value});
   return newRecipes
 }
