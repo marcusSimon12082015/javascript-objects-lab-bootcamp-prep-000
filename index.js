5f38b840eeb8d311cde5e6fca16bacd707330681
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-09-13 16:53:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function updateObjectWithKeyAndValue(object, key, value){
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   var property = key
-  recipes[property] = value
+  object[property] = value
   return recipes
 }
